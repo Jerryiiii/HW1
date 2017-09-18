f91d4c54e2e61954b4dade4e93e0a86338847885
Jerryiiii <31806359+Jerryiiii@users.noreply.github.com>
2017-09-17 18:05:52 -0700

    Create puzzle-4.js

---
+++ puzzle-4.js
@@ -0,0 +1,17 @@
+down
+down
+down
+down
+right
+save getColor as firstColorSeen
+down 
+setColor remember firstColorSeen
+right
+right 
+right
+up
+save getColor as secondColorSeen
+down
+setColor remember secondColorSeen
+right
+right
