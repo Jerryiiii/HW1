61881cab28aa33774033047cde28250e4f3012a8
Jerryiiii <31806359+Jerryiiii@users.noreply.github.com>
2017-09-17 18:07:18 -0700

    Create puzzle-7.js

---
+++ puzzle-7.js
@@ -0,0 +1,20 @@
+recipe pinkRight
+|  if getColor is pink
+   |  right
+   |  right
+recipe downToPink
+|  repeat if getColor isn’t pink
+   |  down
+recipe upToPink
+|  repeat if getColor isn’t pink
+   |  up
+downToPink
+pinkRight
+upToPink
+pinkRight
+downToPink
+pinkRight
+upToPink
+pinkRight
+downToPink
+right
