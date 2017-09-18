f8c2646b955b253f52f71018bba014f16c02b124
Jerryiiii <31806359+Jerryiiii@users.noreply.github.com>
2017-09-17 18:06:24 -0700

    Create puzzle-5.js

---
+++ puzzle-5.js
@@ -0,0 +1,16 @@
+recipe rightTwice
+|  right
+|  right
+recipe ColorWay
+|  if getColor is blue
+   |  down
+   else
+   | up
+rightTwice
+ColorWay
+rightTwice
+ColorWay
+rightTwice
+ColorWay
+rightTwice
+right
